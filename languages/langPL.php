28a1b0e2568955956d8f55171eed64c7a22c39e6
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-11-03 13:41:32 +0100

    space tag added

--- languages/langPL.php
+++ languages/langPL.php
@@ -15,7 +15,7 @@ $lang['MAIN_TITLE'] = 'Bankomat/Wpłatomat';
 
 //PLACEHOLDERS CONTENT
 
-$lang['USER_CARD_LOGIN_CONTENT'] = 'Numer karty';
+$lang['USER_CARD_LOGIN_CONTENT'] = 'Numer&nbsp;karty';
 $lang['USER_PIN_LOGIN_CONTENT'] = 'PIN';
 $lang['PAYOUT_CONTENT'] = 'Kwota';
 $lang['DEPOSIT_CONTENT'] = 'Kwota';
@@ -26,13 +26,13 @@ $lang['BUTTON_SUBMIT_CONTENT'] = 'Zatwierdź';
 $lang['BUTTON_DEPOSIT_CONTENT'] = 'Wpłać';
 $lang['BUTTON_PAYOUT_CONTENT'] = 'Wypłać';
 $lang['BUTTON_LOGOUT_CONTENT'] = 'Wyloguj';
-$lang['BUTTON_MAINPAGEBACK_CONTENT'] = 'Strona główna';
+$lang['BUTTON_MAINPAGEBACK_CONTENT'] = 'Strona&nbsp;główna';
 
 //FOOTER
 
-$lang['FOOTER_PARAGRAPH1_CONTENT'] = 'Bankomat firmy ';
-$lang['FOOTER_PARAGRAPH2_CONTENT'] = ' zrób to sam';
-$lang['COMPANY_NAME'] = 'DOIT©';
-$lang['RIGHT_RESERVED'] = '©2020 All rights reserved.';
+$lang['FOOTER_PARAGRAPH1_CONTENT'] = 'Bankomat&nbsp;firmy';
+$lang['FOOTER_PARAGRAPH2_CONTENT'] = '&nbsp;zrób&nbsp;to&nbsp;sam';
+$lang['COMPANY_NAME'] = '&nbsp;DOIT©';
+$lang['RIGHT_RESERVED'] = '©2020&nbsp;All&nbsp;rights&nbsp;reserved.';
 
 ?>
