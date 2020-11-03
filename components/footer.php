b6d06b632d954f0b48241b17cc5333927cbe1703
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-11-03 13:06:28 +0100

    change extension files in components

---
+++ components/footer.php
@@ -0,0 +1,8 @@
+<?php
+    include 'helpers/common.php';
+?>
+
+<footer>
+    <p>Bankomat firmy <span>DoIt&copy</span> zrób to sam</p>
+    <p><?php echo $lang['RIGHT_RESERVED']; ?></p>
+</footer>
--- components/headerForSubpages.php
+++ components/headerForSubpages.php
@@ -1,6 +1,10 @@
+<?php
+    include 'helpers/common.php';
+?>
+
 <header>
     <div class="logo">
         <img src="../images/logo.svg" alt="Logo Bankomatu/Wpłatomatu">
     </div>
-    <h1>Bankomat/Wpłatomat</h1>
+    <h1><?php echo $lang['MAIN_TITLE']; ?></h1>
 </header>
