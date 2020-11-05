bdc6fb3b7963f88a90c30c4877571a1ae17d2dda
KamilKaczmarek <51368241+KamilKaczmarek@users.noreply.github.com>
2020-11-05 09:08:21 +0100

    Create langRU.php

---
+++ langRU.php
@@ -0,0 +1,38 @@
+<?php
+/* 
+-----------------
+Language: Russian
+-----------------
+*/
+
+$lang = array();
+
+//HEADER
+
+$lang['PAGE_TITLE'] = 'Банкомат/Депозитная машина';
+
+$lang['MAIN_TITLE'] = 'Банкомат/Депозитная машина';
+
+//PLACEHOLDERS CONTENT
+
+$lang['USER_CARD_LOGIN_CONTENT'] = 'Номер&nbsp;карты';
+$lang['USER_PIN_LOGIN_CONTENT'] = 'PIN';
+$lang['PAYOUT_CONTENT'] = 'Количество';
+$lang['DEPOSIT_CONTENT'] = 'Количество';
+
+//BUTTONS CONTENT
+
+$lang['BUTTON_SUBMIT_CONTENT'] = 'представить';
+$lang['BUTTON_DEPOSIT_CONTENT'] = 'Пожертвовать';
+$lang['BUTTON_PAYOUT_CONTENT'] = 'Снять&nbsp;со&nbsp;счета';
+$lang['BUTTON_LOGOUT_CONTENT'] = 'Выйти';
+$lang['BUTTON_MAINPAGEBACK_CONTENT'] = 'Дом';
+
+//FOOTER
+
+$lang['FOOTER_PARAGRAPH1_CONTENT'] = 'Банкомат&nbsp;компании';
+$lang['FOOTER_PARAGRAPH2_CONTENT'] = '&nbsp;сделай&nbsp;это&nbsp;сам';
+$lang['COMPANY_NAME'] = '&nbsp;DOIT©';
+$lang['RIGHT_RESERVED'] = '©2020&nbsp;All&nbsp;rights&nbsp;reserved.';
+
+?>
