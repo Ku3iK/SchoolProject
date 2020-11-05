7516605c47f586cb70ccac1c16243173d1f156a5
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-11-05 08:55:45 +0100

    navigation created

--- components/header.php
+++ components/header.php
@@ -1,3 +1,8 @@
+<nav>
+    <a href="index.php?lang=pl"><img src="./images/poland.svg" alt="Poland"></a>
+    <a href="index.php?lang=en"><img src="./images/united-kingdom.svg" alt="United Kingdom"></a>
+    <a href="index.php?lang=de"><img src="./images/germany.svg" alt="Germany"></a>
+</nav>
 <header>
     <div class="logo">
         <img src="images/logo.svg" alt="Logo Bankomatu/Wpłatomatu">
