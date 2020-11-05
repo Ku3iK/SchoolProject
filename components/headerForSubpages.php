75b63cd19568ee03c9864d614944321db7e47b0c
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2020-11-05 08:55:29 +0100

    Update headerForSubpages.php

--- components/headerForSubpages.php
+++ components/headerForSubpages.php
@@ -1,6 +1,6 @@
 <header>
     <div class="logo">
-        <img src="../images/logo.svg" alt="Logo Bankomatu/Wpłatomatu">
+        <img src="images/logo.svg" alt="Logo Bankomatu/Wpłatomatu">
     </div>
     <h1>Bankomat/Wpłatomat</h1>
 </header>
